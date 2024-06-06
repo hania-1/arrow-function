bdaa10457f4fd28e6b9f849518751d029d41c8ef
hania-1 <160575495+hania-1@users.noreply.github.com>
2024-06-06 22:33:59 +0500

    Update main.js
    removing comments

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //Arrow function
-// let ArrFun = ( name : string, age : number) => console.log (name + " " + age)
-// ArrFun ("hania" , 18)
+let ArrFun = ( name : string, age : number) => console.log (name + " " + age)
+ArrFun ("hania" , 18)
 // //*** /
 // //using "this" in arrow function
-// let h = {
-//     name : "hania",
-//     age : 18,
-//     class : 12,
-//     show: function () {
-// setTimeout( () => {
-//     console.log (`my name is ${this.name}`)
-// }, 2000)
-//     }
-// }
-// h.show ()
+let h = {
+    name : "hania",
+    age : 18,
+    class : 12,
+    show: function () {
+setTimeout( () => {
+    console.log (`my name is ${this.name}`)
+}, 2000)
+    }
+}
+h.show ()
 var addnum = function (num1, num2) { return num1 + num2; };
 //console.log(`addition of ${addnum}`)
 console.log(addnum(2, 4));
